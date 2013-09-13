3622903494071648153e8b3354badd3b30c97739
Anton Podviaznikov <podviaznikov@gmail.com>
2013-09-13 00:26:11 -0300

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hashids
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/hashids/core_test.clj
@@ -0,0 +1,7 @@
+(ns hashids.core-test
+  (:use clojure.test
+        hashids.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
