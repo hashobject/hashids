e792b29aea728e1366e8d514d1eed4a8df97b2cf
Anton Podviaznikov <podviaznikov@gmail.com>
2013-09-13 00:51:04 -0300

    documentation

--- src/clojure/hashids/core.clj
+++ src/clojure/hashids/core.clj
@@ -1,8 +1,11 @@
 (ns hashids.core
+  "Clojure wrapper for Hashids"
   (:import (hashids Hashids)))
 
 (defn encrypt [id salt]
+  "Encrypt number into string using salt"
   (.encrypt (Hashids. salt) (long id)))
 
 (defn decrypt [code salt]
+  "Decrypt number from the string using salt"
   (first (.decrypt (Hashids. salt) code)))
