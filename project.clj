898ffa0c5cdc5076f01b874f3a0d4dbc7d63d319
Anton Podviaznikov <podviaznikov@gmail.com>
2014-12-28 00:48:51 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :url "https://github.com/hashobject/hashids"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"])
